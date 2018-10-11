887221b5d5251f8978e31503334e2a497c98291b
unknown <binhaibin>
2018-10-11 11:42:09 +0700

    second

---
+++ File/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo7
+{
+    class User
+    {
+        private string _name;
+        private string _email;
+        private string _sdt;
+        private string _dc;
+
+        public string name { get => _name; set => _name = value; }
+        public string email { get => _email; set => _email = value; }
+        public string sdt { get => _sdt; set => _sdt = value; }
+        public string dc { get => _dc; set => _dc = value; }
+    }
+}
---
+++ File/user.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace demo7
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class user : Page
+    {
+        public user()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Save(object sender, RoutedEventArgs e)
+        {
+            User user = new User();
+            user.name = this.name.Text;
+            user.email = this.email.Text;
+            user.sdt = this.sdt.Text;
+            user.dc = this.dc.Text;
+            string Jsonmember = JsonConvert.SerializeObject(user);
+
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until
+                // we finish making changes and call CompleteUpdatesAsync.
+                Windows.Storage.CachedFileManager.DeferUpdates(file);
+                // write to file
+                await Windows.Storage.FileIO.WriteTextAsync(file, Jsonmember);
+                // Let Windows know that we're finished changing the file so
+                // the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                Windows.Storage.Provider.FileUpdateStatus status =
+                    await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                {
+                    this.mess.Text = "File " + file.Name + " was saved.";
+                }
+                else
+                {
+                    this.mess.Text = "File " + file.Name + " couldn't be saved.";
+                }
+            }
+            else
+            {
+                this.mess.Text = "Operation cancelled.";
+            }
+        }
+
+
+
+        private async void bth_load(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            picker.FileTypeFilter.Add(".txt");
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var json = await FileIO.ReadTextAsync(file);
+                var Us = JsonConvert.DeserializeObject<User>(json);
+                this.name.Text = Us.name;
+                this.email.Text = Us.email;
+                this.sdt.Text = Us.sdt;
+                this.dc.Text = Us.dc;
+            }
+        }
+    }
+}
